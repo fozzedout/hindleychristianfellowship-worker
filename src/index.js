66f2b8b794b146c9fdab668a70f8e0bfe20f13d3
Jaxah <fozzedout@gmail.com>
2025-03-22 20:07:23 +0000

    Added email sending test

--- src/index.js
+++ src/index.js
@@ -8,8 +8,35 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { EmailMessage } from "cloudflare:email";
+import { createMimeMessage } from "mimetext";
+
 export default {
 	async fetch(request, env, ctx) {
+
+
+		const sender = "contact@hindleychristianfellowship.co.uk";
+		const recipient = "fozzedout@gmail.com";
+
+		const msg = createMimeMessage();
+		msg.setSender({ name: "HCF", addr: sender });
+		msg.setRecipient( recipient );
+		msg.setSubject( "An email generated in a worker" );
+		msg.addMessage({
+			contentType: "text/plain",
+			data: `Congratulations, you just sent an email!`
+		});
+
+		var message = new EmailMessage( sender, recipient, msg.asRaw() );
+
+		try {
+			await env.emailBinding.send( message );
+		} catch (e) {
+			return new Response( e.message );
+		}
+
+
 		return new Response('He is not here, He is risen!');
 	},
 };
+
