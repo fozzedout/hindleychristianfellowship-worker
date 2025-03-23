1463277a28abb70ee48b10f4e4aeae6a29c9e5a4
Jaxah <fozzedout@gmail.com>
2025-03-23 07:10:33 +0000

    testing types of submission

--- src/index.js
+++ src/index.js
@@ -14,13 +14,72 @@ import { createMimeMessage } from "mimetext";
 export default {
 	async fetch(request, env, ctx) {
 
+		    /**
+     * rawHtmlResponse returns HTML inputted directly
+     * into the worker script
+     * @param {string} html
+     */
+			function rawHtmlResponse(html) {
+				return new Response(html, {
+				  headers: {
+					"content-type": "text/html;charset=UTF-8",
+				  },
+				});
+			  }
+		
+			  
+    /**
+     * readRequestBody reads in the incoming request body
+     * Use await readRequestBody(..) in an async function to get the string
+     * @param {Request} request the incoming request to read from
+     */
+    async function readRequestBody(request) {
+		const contentType = request.headers.get("content-type");
+		if (contentType.includes("application/json")) {
+		  return JSON.stringify(await request.json());
+		} else if (contentType.includes("application/text")) {
+		  return request.text();
+		} else if (contentType.includes("text/html")) {
+		  return request.text();
+		} else if (contentType.includes("form")) {
+		  const formData = await request.formData();
+		  const body = {};
+		  for (const entry of formData.entries()) {
+			body[entry[0]] = entry[1];
+		  }
+		  return JSON.stringify(body);
+		} else {
+		  // Perhaps some other type of data was submitted in the form
+		  // like an image, or some other binary data.
+		  return "a file";
+		}
+	  }
+
+
+	  const { url } = request;
+	  if (url.includes("form")) {
+		return rawHtmlResponse(someForm);
+	  }
+	  if (request.method === "POST") {
+		const reqBody = await readRequestBody(request);
+		const retBody = `The request body sent in was ${reqBody}`;
+		return new Response(retBody);
+	  } else if (request.method === "GET") {
+		return new Response("The request was a GET");
+	  }
+
 		if ( request.method !== "POST" ) {
 			return new Response('He is risen!');
 		}
 
-		//const data = await request.json();
+		try {
+			const data = await request.json();
+		} catch (error) {
+			return new Response( error.message );
 
+		}
 
+/*
 		const sender = "contact@hindleychristianfellowship.co.uk";
 		const recipient = "fozzedout@gmail.com";
 
@@ -37,10 +96,10 @@ export default {
 
 		try {
 			await env.emailBinding.send( message );
-		} catch (e) {
-			return new Response( e.message );
+		} catch (error) {
+			return new Response( error.message );
 		}
-
+*/
 
 		return new Response('He is not here, He is risen!');
 	},
