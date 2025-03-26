57094bc7af09f02bc15e0c7b48bd6c88ee8e4090
Jaxah <fozzedout@gmail.com>
2025-03-26 03:04:22 +0000

    added all the emailing functionality

--- src/index.js
+++ src/index.js
@@ -12,42 +12,66 @@ import { EmailMessage } from "cloudflare:email";
 import { createMimeMessage } from "mimetext";
 
 const corsHeaders = {
-  'Access-Control-Allow-Headers': '*', // What headers are allowed. * is wildcard. Instead of using '*', you can specify a list of specific headers that are allowed, such as: Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Authorization.
-  'Access-Control-Allow-Methods': 'GET,POST,HEAD', // Allowed methods. Others could be GET, PUT, DELETE etc.
-  'Access-Control-Allow-Origin': '*', // This is URLs that are allowed to access the server. * is the wildcard character meaning any URL can.
+	'Access-Control-Allow-Headers': '*', // What headers are allowed. * is wildcard. Instead of using '*', you can specify a list of specific headers that are allowed, such as: Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Authorization.
+	'Access-Control-Allow-Methods': 'GET,POST,HEAD', // Allowed methods. Others could be GET, PUT, DELETE etc.
+	'Access-Control-Allow-Origin': '', // This is URLs that are allowed to access the server. * is the wildcard character meaning any URL can.
+}
+
+const corsWhitelist = [
+	"https://hindleychristianfellowship.co.uk",
+	"https://hindleychristianfellowship.uk",
+	"https://hartcommonchurch.co.uk",
+	"https://hartcommonchurch.uk",
+];
+
+const sendEmail = (name, email, message) => {
+	const sender = "contact@hindleychristianfellowship.co.uk";
+	const recipient = "fozzedout@gmail.com";
+
+	const msg = createMimeMessage();
+	msg.setSender({ name: "Hindley Christian Fellowship", addr: sender });
+	msg.setRecipient( recipient );
+	msg.setSubject( "Hindley Christian Fellowship Website Contact" );
+	msg.addMessage({
+		contentType: "text/plain",
+		data: `Name: ${name}
+Email: ${email}
+Comment: ${message}`
+	});
+
+	var message = new EmailMessage( sender, recipient, msg.asRaw() );
+
+	try {
+		await env.emailBinding.send( message );
+	} catch (error) {
+		return error.message;
+	}
+
+	return "Thank you! We've received your message and will get back to you soon.";
 }
 
 
 export default {
 	async fetch(request, env, ctx) {
+		if ( !corsWhitelist.includes(request.headers.origin) ) {
+			return new Response( "Invalid header" );
+		}
+
+		let data;
 		try {
-			const data = await request.json();
+			data = await request.json();
 		} catch (error) {
 			return new Response( error.message, { headers: corsHeaders } );
-
 		}
 
-/*
-		const sender = "contact@hindleychristianfellowship.co.uk";
-		const recipient = "fozzedout@gmail.com";
+		corsHeaders["Access-Control-Allow-Origin"] = request.headers.origin;
 
-		const msg = createMimeMessage();
-		msg.setSender({ name: "Hindley Christian Fellowship", addr: sender });
-		msg.setRecipient( recipient );
-		msg.setSubject( "" );
-		msg.addMessage({
-			contentType: "text/plain",
-			data: `Congratulations, you just sent an email!`
-		});
-
-		var message = new EmailMessage( sender, recipient, msg.asRaw() );
-
-		try {
-			await env.emailBinding.send( message );
-		} catch (error) {
-			return new Response( error.message );
+		if ( request.method === "POST" ) {
+			return new Response(
+				sendEmail(data.name, data.email, data.comment),
+				{ headers: corsHeaders }
+			);
 		}
-*/
 
 		return new Response('He is not here, He is risen!', { headers: corsHeaders });
 	},
