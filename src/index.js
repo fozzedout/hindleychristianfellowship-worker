c2fbc5fe76d006f26516d55c17f35c5a7c5c61fb
Jaxah <fozzedout@gmail.com>
2025-03-26 03:30:01 +0000

    added OPTIONS handling due to CORs requirement

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { createMimeMessage } from "mimetext";
 
 const corsHeaders = {
 	'Access-Control-Allow-Headers': '*', // What headers are allowed. * is wildcard. Instead of using '*', you can specify a list of specific headers that are allowed, such as: Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Authorization.
-	'Access-Control-Allow-Methods': 'GET,POST,HEAD', // Allowed methods. Others could be GET, PUT, DELETE etc.
+	'Access-Control-Allow-Methods': 'GET,POST,HEAD,OPTIONS', // Allowed methods. Others could be GET, PUT, DELETE etc.
 	'Access-Control-Allow-Origin': '', // This is URLs that are allowed to access the server. * is the wildcard character meaning any URL can.
 }
 
@@ -66,7 +66,15 @@ export default {
 
 		corsHeaders["Access-Control-Allow-Origin"] = request.headers.origin;
 
-		if ( request.method === "POST" ) {
+		// Handle OPTIONS preflight request
+		if (request.method === 'OPTIONS') {
+			event.respondWith(
+				new Response(null, {
+					status: 204,
+					headers: corsHeaders,
+				})
+			);
+		} else if ( request.method === "POST" ) {
 			return new Response(
 				sendEmail(data.name, data.email, data.comment),
 				{ headers: corsHeaders }
